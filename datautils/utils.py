ee422470f4eb4184ed73f55bff9e49c3751f4547
lihang <lihang@qq.com>
2018-01-22 19:55:22 +0800

    ddd

--- datautils/utils.py
+++ datautils/utils.py
@@ -2,42 +2,38 @@ import codecs
 import numpy as np
 import jieba
 import h5py,shelve
-'''
-f = h5py.File('wordData50.hdf5','r')
-X = f['X'].value
-Y = f['Y'].value
-d = shelve.open('wordData50.data')
-Vocab = d['Vocab']
-id2c = d['id2c']
-c2id = d['c2id']
-#print(X,Y,Vocab,id2c,c2id)
-'''
 class Data():
     def __init__(self,datafilepath):
         file = codecs.open(datafilepath,'r').readlines()[1:]
-        file = [i.replace('\n','') for i in file]
         files = [i.split(',') for i in file]
         self.id,self.X,self.Y = [],[],[]
         for idx,i in enumerate(file):
             self.id.append(files[idx][0])
             idlen = len(files[idx][0])
-            self.X.append(i[idlen+1:])
-        print(self.id[0],self.X[0])
-    
-    def makechar(self,padlen=50):
-        d = shelve.open('charData50.data')
-        Vocab = d['Vocab']
-        id2c = d['id2c']
-        c2id = d['c2id']
+            self.X.append(i[idlen+1:-3])
+            self.Y.append(i[-2:-1])
+        print(self.id[0],self.X[0],self.Y[0])
+
+    def chardata(self,padlen=50):
+        #count char number
+        charVocab = set()
+        for i in self.X:
+            for j in i:
+                charVocab.add(j)
+        charVocabSize = len(charVocab)
+        #complete dict for id and char
+        id2char = {}
+        char2id = {}
+        for idx,i in enumerate(charVocab):
+            id2char[idx+1] = i
+            char2id[i] = idx+1
+        #make
         charX = []
+        charY = []
         for i in self.X:
             tmpline = []
             for j in i:
-                tmpid = 0
-                try:
-                    tmpid = c2id[j]
-                except Exception:
-                    pass
+                tmpid = char2id[j]
                 tmpline.append(tmpid)
             pad = padlen-len(tmpline)
             for k in range(0,pad):
@@ -45,18 +41,11 @@ class Data():
             tmpline = tmpline[0:padlen]
             charX.append(np.array(tmpline))
         charX = np.array(charX)
-        return charX
+        charY = [int(i) for i in self.Y]
+        charY = np.array(charY)
+        charY = np.reshape(charY,(-1,))
+        return charVocab,id2char,char2id,charX,charY
 
-    def savechar(self,padlen):
-        charX = self.makechar()
-        f = h5py.File("charTest"+str(padlen)+".hdf5","w")
-        f.create_dataset('X',data=charX)
-        f.close()        
-        d = shelve.open("idTest"+str(padlen))
-        d['id'] = self.id
-        d.close()
-        print(len(self.X),len(self.id))
-    
     def getstopword(self):
         stopwords = set(['的','我'])
         return stopwords
@@ -102,6 +91,19 @@ class Data():
         wordY = np.reshape(wordY,(-1,))
         return wordVocab,id2word,word2id,wordX,wordY
 
+    def savechar(self,padlen):
+        charVocab,id2char,char2id,charX,charY = self.chardata()
+        print('charVocab:',len(charVocab),'sampleX:',len(charX))
+        #print(charVocab,id2char,char2id,charY,charX)
+        f = h5py.File("charData"+str(padlen)+".hdf5","w")
+        f.create_dataset('X',data=charX)
+        f.create_dataset('Y',data=charY)
+        d = shelve.open("charData"+str(padlen)+".data")
+        d['Vocab'] = charVocab
+        d['id2c'] = id2char
+        d['c2id'] = char2id
+        d.close()
+        f.close()
     def saveword(self,padlen):
         wordVocab,id2word,word2id,wordX,wordY = self.worddata()
         print('wordVocab:',len(wordVocab),'sampleX:',len(wordX))
@@ -114,15 +116,15 @@ class Data():
         d['id2c'] = id2word
         d['c2id'] = word2id
         f.close()
-    
+
     def save(self,padlen=50):
         print('start....')
         self.savechar(padlen=padlen)
         print('save char done....')
-        #self.saveword(padlen=padlen)
-        #print('save word done....')
+        self.saveword(padlen=padlen)
+        print('save word done....')
 
-data = Data('YNU/predict_first.csv')
+data = Data('YNU/train_first.csv')
 data.save()
 
 #test:
@@ -135,4 +137,3 @@ Vocab = d['Vocab']
 id2c = d['id2c']
 c2id = d['c2id']
 #print(X,Y,Vocab,id2c,c2id)
-'''
